02b022fad80df22eafd120bef8fce593a79d13fc
Ibe Ezenwa Augustine <austinibe15@gmail.com>
2024-11-07 04:43:41 -0800

    PLP Python Assignment1

---
+++ user_input.py
@@ -0,0 +1,8 @@
+
+# Asking for user input  
+name = input("What is your name? ")  
+age = input("How old are you? ")  
+location = input("Where do you live? ")  
+
+# Printing a personalized message  
+print(f"Hello {name}, you are {age} years old and live in {location}.")
